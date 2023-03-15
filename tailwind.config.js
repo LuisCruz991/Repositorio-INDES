757e081b1bf05f1b8ebfc0e6cc7544436cdc0929
Walter Calderon <20180082@ricaldone.edu.sv>
2023-03-14 19:38:01 -0600

    Dashboard
    Creacion del dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,21 @@ module.exports = {
         'btn-hover': '#548CA8',
         'azul-1': '#6B728E',
         'azul-2': '#476072',
+        'azul-opaco': '#E2E8F0',
       },
       fontFamily:{
-        overpass:['overpass']
+        overpass:['overpass'],
+        jakarta:['plus jakarta sans']
+      },
+      width: {
+        '124': '28rem',
+        '126': '30rem',
+        '128': '32rem',
+      },
+      height: {
+        '124': '28rem',
+        '126': '30rem',
+        '128': '32rem',
       },
     },
   },
