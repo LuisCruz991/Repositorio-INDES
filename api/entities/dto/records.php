ab3e7e3e4171fcdf96374c0819a4a5b9d200daf2
LuisCruz991 <greninjaash1000@gmail.com>
2023-06-02 11:04:53 -0600

    records_crud

---
+++ api/entities/dto/records.php
@@ -0,0 +1,113 @@
+<?php
+require_once('../../helpers/validator.php');
+require_once('../../entities/dao/records_queries.php');
+/*
+*	Clase para manejar la transferencia de datos de la entidad RECORDS.
+*/
+class Records extends RecordsQueries
+{
+    // Declaración de atributos (propiedades).
+    protected $id = null;
+    protected $marca_obtenida = null;
+    protected $unidad_medida = null;
+    protected $atleta = null;
+    protected $prueba = null;
+    protected $posicion = null;
+
+    /*
+    *   Métodos para validar y asignar valores de los atributos.
+    */
+    public function setId($value)
+    {
+        if (Validator::validateNaturalNumber($value)) {
+            $this->id = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setMarcaObtenida($value)
+    {
+        if (Validator::validateAlphabetic($value, 1, 50)) {
+            $this->marca_obtenida = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setUnidadMedida($value)
+    {
+        if (Validator::validateNaturalNumber($value)) {
+            $this->unidad_medida = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public function setAtleta($value)
+    {
+        if (Validator::validateNaturalNumber($value)) {
+            $this->atleta = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setPrueba($value)
+    {
+        if (Validator::validateNaturalNumber($value)) {
+            $this->prueba = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setPosicion($value)
+    {
+        if (Validator::validateNaturalNumber($value)) {
+            $this->posicion = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /*
+    *   Métodos para obtener valores de los atributos.
+    */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getMarcaObtenida()
+    {
+        return $this->marca_obtenida;
+    }
+
+    public function getUnidadMedida()
+    {
+        return $this->unidad_medida;
+    }
+
+    public function getAtleta()
+    {
+        return $this->atleta;
+    }
+
+    public function getPrueba()
+    {
+        return $this->prueba;
+    }
+
+    public function getPosicion()
+    {
+        
+        return $this->posicion;
+    }
+}
