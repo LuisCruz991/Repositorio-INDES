79b775a1221c2f980cc64a835497b98e16383399
JosuePortillo1 <82895807+JosuePortillo1@users.noreply.github.com>
2023-06-15 09:43:35 -0600

    Backend de modalidad deportiva

---
+++ api/business/modalidad.php
@@ -0,0 +1,100 @@
+<?php
+require_once('../../entities/dto/modalidad.php');
+
+// Se comprueba si existe una acción a realizar, de lo contrario se finaliza el script con un mensaje de error.
+if (isset($_GET['action'])) {
+    // Se crea una sesión o se reanuda la actual para poder utilizar variables de sesión en el script.
+    session_start();
+    // Se instancia la clase correspondiente.
+    $modalidad = new modalidad;
+    // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
+    $result = array('status' => 0, 'message' => null, 'exception' => null, 'dataset' => null);
+    // Se verifica si existe una sesión iniciada como administrador, de lo contrario se finaliza el script con un mensaje de error.
+    if (isset($_SESSION['id_usuario'])) {
+        // Se compara la acción a realizar cuando un administrador ha iniciado sesión.
+        switch ($_GET['action']) {
+            case 'readAll':
+                if ($result['dataset'] = $modalidad->readAll()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Existen ' . count($result['dataset']) . ' registros';
+                } elseif (Database::getException()) {
+                    $result['exception'] = Database::getException();
+                } else {
+                    $result['exception'] = 'No hay datos registrados';
+                }
+                break;
+            case 'search':
+                $_POST = Validator::validateForm($_POST);
+                if ($_POST['search'] == '') {
+                    $result['exception'] = 'Ingrese un valor para buscar';
+                } elseif ($result['dataset'] = $modalidad->searchRows($_POST['search'])) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Existen ' . count($result['dataset']) . ' coincidencias';
+                } elseif (Database::getException()) {
+                    $result['exception'] = Database::getException();
+                } else {
+                    $result['exception'] = 'No hay coincidencias';
+                }
+                break;
+            case 'create':
+                $_POST = Validator::validateForm($_POST);
+                if (!$modalidad->setModalidad($_POST['modalidad'])) {
+                    $result['exception'] = 'Modalidad deportiva no valida';
+                } elseif ($modalidad->createRow()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Modalidad deportiva guardada exitosamente';
+                } else {
+                    $result['exception'] = Database::getException();
+                }
+                break;
+            case 'readOne':
+                if (!$modalidad->setId($_POST['id'])) {
+                    $result['exception'] = 'Modalidad no invalida';
+                } elseif ($result['dataset'] = $modalidad->readOne()) {
+                    $result['status'] = 1;
+                } elseif (Database::getException()) {
+                    $result['exception'] = Database::getException();
+                } else {
+                    $result['exception'] = 'Ocurrió un problema al leer la modalidad';
+                }
+                break;
+            case 'update':
+                $_POST = Validator::validateForm($_POST);
+                if (!$modalidad->setId($_POST['id'])) {
+                    $result['exception'] = 'Modalidad invalida';
+                } elseif (!$data = $modalidad->readOne()) {
+                    $result['exception'] = 'Ocurrió un problema al leer la modalidad';
+                }elseif (!$modalidad->setModalidad($_POST['modalidad'])) {
+                    $result['exception'] = 'nombre de la modalidad invalida';
+                } elseif ($modalidad->updateRow()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Modalidad actualizada exitosamente';
+                } else {
+                    $result['exception'] = Database::getException();
+                }
+                break;
+            case 'delete':
+                if (!$modalidad->setId($_POST['id'])) {
+                    $result['exception'] = 'Modalidad invalida';
+                } elseif (!$data = $modalidad->readOne()) {
+                    $result['exception'] = 'Ocurrió un problema al leer la modalidad';
+                } elseif ($modalidad->deleteRow()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Modalidad descartada exitosamente';
+                } else {
+                    $result['exception'] = Database::getException();
+                }
+                break;
+            default:
+                $result['exception'] = 'Acción no disponible dentro de la sesión';
+        }
+        // Se indica el tipo de contenido a mostrar y su respectivo conjunto de caracteres.
+        header('content-type: application/json; charset=utf-8');
+        // Se imprime el resultado en formato JSON y se retorna al controlador.
+        print(json_encode($result));
+    } else {
+        print(json_encode('Acceso denegado'));
+    }
+} else {
+    print(json_encode('Recurso no disponible'));
+}
---
+++ api/entities/dto/modalidad.php
@@ -0,0 +1,44 @@
+<?php
+require_once('../../helpers/validator.php');
+require_once('../../entities/dao/modalida_queries.php');
+
+
+class modalidad extends ModalidadQueries  {
+
+    protected $id = null;
+    protected $modalidad = null ;
+
+
+
+
+    public function setId($value)
+    {
+        if (Validator::validateNaturalNumber($value)) {
+            $this->id = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setModalidad($value)
+    {
+        if (Validator::validateAlphabetic($value, 1, 50)) {
+            $this->modalidad = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getModalidad()
+    {
+        return $this->modalidad;
+    }
+
+}
