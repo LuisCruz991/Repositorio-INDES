cbbf2d236db3b988a7dbfe3aa33cf78e3d7e61c9
LuisCruz991 <greninjaash1000@gmail.com>
2023-06-15 12:07:04 -0600

    records y pruebas

--- api/business/dashboard/prueba.php
+++ api/business/dashboard/prueba.php
@@ -1,12 +1,12 @@
 <?php
-require_once('../../entities/dto/records.php');
+require_once('../../entities/dto/pruebas.php');
 
 // Se comprueba si existe una acción a realizar, de lo contrario se finaliza el script con un mensaje de error.
 if (isset($_GET['action'])) {
     // Se crea una sesión o se reanuda la actual para poder utilizar variables de sesión en el script.
     session_start();
     // Se instancia la clase correspondiente.
-    $record = new Record;
+    $prueba = new Prueba;
     // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
     $result = array('status' => 0, 'message' => null, 'exception' => null, 'dataset' => null);
     // Se verifica si existe una sesión iniciada como administrador, de lo contrario se finaliza el script con un mensaje de error.
@@ -14,7 +14,7 @@ if (isset($_GET['action'])) {
         // Se compara la acción a realizar cuando un administrador ha iniciado sesión.
         switch ($_GET['action']) {
             case 'readAll':
-                if ($result['dataset'] = $record->readAll()) {
+                if ($result['dataset'] = $prueba->readAll()) {
                     $result['status'] = 1;
                 } elseif (Database::getException()) {
                     $result['exception'] = Database::getException();
@@ -26,7 +26,7 @@ if (isset($_GET['action'])) {
                 $_POST = Validator::validateForm($_POST);
                 if ($_POST['search'] == '') {
                     $result['exception'] = 'Ingrese un valor para buscar';
-                } elseif ($result['dataset'] = $record->searchRows($_POST['search'])) {
+                } elseif ($result['dataset'] = $prueba->searchRows($_POST['search'])) {
                     $result['status'] = 1;
                     $result['message'] = 'Existen '.count($result['dataset']).' coincidencias';
                 } elseif (Database::getException()) {
@@ -37,57 +37,53 @@ if (isset($_GET['action'])) {
                 break;
             case 'create':
                 $_POST = Validator::validateForm($_POST);
-                if (!$record->setMarcaObtenida($_POST['marca_obtenida'])) {
-                    $result['exception'] = 'Marca incorrecta';
-                }  elseif (!$record->setUnidadMedida($_POST['unidad_medida'])) {
-                    $result['exception'] = 'Unidad de medida incorrecta';
-                }  elseif (!$record->setAtleta($_POST['atleta'])) {
-                    $result['exception'] = 'Atleta incorrecto';
-                }   elseif (!$record->setPrueba($_POST['prueba'])) {
-                    $result['exception'] = 'Prueba incorrecta';
-                }  elseif (!$record->setPosicion($_POST['posicion'])) {
-                    $result['exception'] = 'Posicion incorrecta';
-                }  elseif ($record->createRow()) {
+                if (!$prueba->setNombre($_POST['nombre'])) {
+                    $result['exception'] = 'Nombre incorrecto';
+                }  elseif (!$prueba->setDeporte($_POST['deporte'])) {
+                    $result['exception'] = 'Deporte incorrecto';
+                }  elseif (!$prueba->setEvento($_POST['evento'])) {
+                    $result['exception'] = 'Evento incorrecto';
+                }  elseif ($prueba->createRow()) {
                     $result['status'] = 1;
-                    $result['message'] = 'Record creado correctamente';
+                    $result['message'] = 'Prueba creada correctamente';
                 } else {
                     $result['exception'] = Database::getException();;
                 }
                 break;
             case 'readOne':
-                if (!$record->setId($_POST['id'])) {
-                    $result['exception'] = 'Record incorrecto';
-                } elseif ($result['dataset'] = $record->readOne()) {
+                if (!$prueba->setId($_POST['id'])) {
+                    $result['exception'] = 'Prueba incorrecta';
+                } elseif ($result['dataset'] = $prueba->readOne()) {
                     $result['status'] = 1;
                 } elseif (Database::getException()) {
                     $result['exception'] = Database::getException();
                 } else {
-                    $result['exception'] = 'Record inexistente';
+                    $result['exception'] = 'Prueba inexistente';
                 }
                 break;
             case 'update':
                 $_POST = Validator::validateForm($_POST);
-                if (!$record->setId($_POST['id'])) {
-                    $result['exception'] = 'Record incorrecto';
-                } elseif (!$data = $record->readOne()) {
-                    $result['exception'] = 'Record inexistente';
-                } elseif (!$record->setMarcaObtenida($_POST['marca_obtenida'])) {
-                    $result['exception'] = 'Marca incorrecta';
-                }  elseif ($record->updateRow()) {
+                if (!$prueba->setId($_POST['id'])) {
+                    $result['exception'] = 'Prueba incorrecta';
+                } elseif (!$data = $prueba->readOne()) {
+                    $result['exception'] = 'Prueba inexistente';
+                } elseif (!$prueba->setNombre($_POST['nombre'])) {
+                    $result['exception'] = 'Nombre incorrecto';
+                }  elseif ($prueba->updateRow()) {
                     $result['status'] = 1;
-                    $result['message'] = 'Record modificado correctamente';
+                    $result['message'] = 'Prueba modificada correctamente';
                 }else {
                     $result['exception'] = Database::getException();
                 }
                 break;
             case 'delete':
-                if (!$record->setId($_POST['idrecord'])) {
-                    $result['exception'] = 'Record incorrecto';
-                } elseif (!$record->readOne()) {
-                    $result['exception'] = 'Record inexistente';
-                } elseif ($record->deleteRow()) {
+                if (!$prueba->setId($_POST['idrecord'])) {
+                    $result['exception'] = 'Prueba incorrecta';
+                } elseif (!$prueba->readOne()) {
+                    $result['exception'] = 'Prueba inexistente';
+                } elseif ($prueba->deleteRow()) {
                     $result['status'] = 1;
-                    $result['message'] = 'Record eliminado correctamente';
+                    $result['message'] = 'Prueba eliminada correctamente';
                 } else {
                     $result['exception'] = Database::getException();
                 }
