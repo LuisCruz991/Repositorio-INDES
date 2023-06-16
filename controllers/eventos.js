e617748c3e70d0c7136d583ae6437dee0fa1c2ac
LuisCruz991 <greninjaash1000@gmail.com>
2023-06-16 12:04:29 -0600

    avances

--- controllers/eventos.js
+++ controllers/eventos.js
@@ -1,5 +1,7 @@
-// Constante para completar la ruta de la API.
-const CLASIFICACION_API = 'business/dashboard/clas_deportes.php';
+// Constantes para completar las rutas de la API.
+const EVENTO_API = 'business/evento.php';
+const TIPOEVENTO_API = 'business/.php';
+
 // Constante para establecer el formulario de buscar.
 const SEARCH_FORM = document.getElementById('search-form');
 // Constante para establecer el formulario de guardar.
@@ -14,7 +16,6 @@ const OPTIONS = {
     dismissible: false
 }
 // Inicialización del componente Modal para que funcionen las cajas de diálogo.
-
 // Constante para establecer la modal de guardar.
 const SAVE_MODAL = document.getElementById('save-modal');
 
@@ -43,7 +44,7 @@ SAVE_FORM.addEventListener('submit', async (event) => {
     // Constante tipo objeto con los datos del formulario.
     const FORM = new FormData(SAVE_FORM);
     // Petición para guardar los datos del formulario.
-    const JSON = await dataFetch(CLASIFICACION_API, action, FORM);
+    const JSON = await dataFetch(EVENTO_API, action, FORM);
     // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
     if (JSON.status) {
         // Se carga nuevamente la tabla para visualizar los cambios.
@@ -51,7 +52,6 @@ SAVE_FORM.addEventListener('submit', async (event) => {
         // Se cierra la caja de diálogo.
         // Se muestra un mensaje de éxito.
         sweetAlert(1, JSON.message, true);
-
     } else {
         sweetAlert(2, JSON.exception, false);
     }
@@ -69,35 +69,39 @@ async function fillTable(form = null) {
     // Se verifica la acción a realizar.
     (form) ? action = 'search' : action = 'readAll';
     // Petición para obtener los registros disponibles.
-    const JSON = await dataFetch(CLASIFICACION_API, action, form);
+    const JSON = await dataFetch(EVENTO_API, action, form);
     // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
     if (JSON.status) {
-        // Se recorre el conjunto de registros fila por fila.
+        // Se recorre el conjunto de registros (dataset) fila por fila a través del objeto row.
         JSON.dataset.forEach(row => {
             // Se crean y concatenan las filas de la tabla con los datos de cada registro.
             TBODY_ROWS.innerHTML += `
                 <tr>
-                    <td>${row.idclasificacion_deporte}</td>
-                    <td>${row.nombre_clasificacion}</td>
+                    <td>${row.idevento}</td>
+                    <td>${row.nombre_prueba}</td>
+                    <td>${row.nombre_deporte}</td>
+                    <td>${row.nombre_evento}</td>
                     <td>
-                        <a onclick="openUpdate(${row.idclasificacion_deporte})" data-bs-toggle="modal" data-bs-target="#save-modal" class="btn btn-primary tooltipped" data-tooltip="Actualizar">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-recycle" viewBox="0 0 16 16">
-                        <path d="M9.302 1.256a1.5 1.5 0 0 0-2.604 0l-1.704 2.98a.5.5 0 0 0 .869.497l1.703-2.981a.5.5 0 0 1 .868 0l2.54 4.444-1.256-.337a.5.5 0 1 0-.26.966l2.415.647a.5.5 0 0 0 .613-.353l.647-2.415a.5.5 0 1 0-.966-.259l-.333 1.242-2.532-4.431zM2.973 7.773l-1.255.337a.5.5 0 1 1-.26-.966l2.416-.647a.5.5 0 0 1 .612.353l.647 2.415a.5.5 0 0 1-.966.259l-.333-1.242-2.545 4.454a.5.5 0 0 0 .434.748H5a.5.5 0 0 1 0 1H1.723A1.5 1.5 0 0 1 .421 12.24l2.552-4.467zm10.89 1.463a.5.5 0 1 0-.868.496l1.716 3.004a.5.5 0 0 1-.434.748h-5.57l.647-.646a.5.5 0 1 0-.708-.707l-1.5 1.5a.498.498 0 0 0 0 .707l1.5 1.5a.5.5 0 1 0 .708-.707l-.647-.647h5.57a1.5 1.5 0 0 0 1.302-2.244l-1.716-3.004z"/>
-                        </svg>
-                        </a>
-                    </td> 
-                    <td>
-                        <a onclick="openDelete(${row.idclasificacion_deporte})" class="btn btn-danger tooltipped" data-tooltip="Eliminar">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-                        <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-                        </svg>
-                        </a>
-                    </td> 
+                    <a onclick="openUpdate(${row.idevento})" data-bs-toggle="modal" data-bs-target="#save-modal" class="btn btn-primary tooltipped" data-tooltip="Actualizar">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-recycle" viewBox="0 0 16 16">
+                    <path d="M9.302 1.256a1.5 1.5 0 0 0-2.604 0l-1.704 2.98a.5.5 0 0 0 .869.497l1.703-2.981a.5.5 0 0 1 .868 0l2.54 4.444-1.256-.337a.5.5 0 1 0-.26.966l2.415.647a.5.5 0 0 0 .613-.353l.647-2.415a.5.5 0 1 0-.966-.259l-.333 1.242-2.532-4.431zM2.973 7.773l-1.255.337a.5.5 0 1 1-.26-.966l2.416-.647a.5.5 0 0 1 .612.353l.647 2.415a.5.5 0 0 1-.966.259l-.333-1.242-2.545 4.454a.5.5 0 0 0 .434.748H5a.5.5 0 0 1 0 1H1.723A1.5 1.5 0 0 1 .421 12.24l2.552-4.467zm10.89 1.463a.5.5 0 1 0-.868.496l1.716 3.004a.5.5 0 0 1-.434.748h-5.57l.647-.646a.5.5 0 1 0-.708-.707l-1.5 1.5a.498.498 0 0 0 0 .707l1.5 1.5a.5.5 0 1 0 .708-.707l-.647-.647h5.57a1.5 1.5 0 0 0 1.302-2.244l-1.716-3.004z"/>
+                    </svg>
+                    </a>
+                </td> 
+                <td>
+                    <a onclick="openDelete(${row.idevento})" class="btn btn-danger tooltipped" data-tooltip="Eliminar">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                    </svg>
+                    </a>
+                    </td>
                 </tr>
             `;
         });
+        // Se inicializa el componente Material Box para que funcione el efecto Lightbox.
         // Se inicializa el componente Tooltip para que funcionen las sugerencias textuales.
-     
+        // Se muestra un mensaje de acuerdo con el resultado.
+        RECORDS.textContent = JSON.message;
     } else {
         sweetAlert(4, JSON.exception, true);
     }
@@ -110,11 +114,14 @@ async function fillTable(form = null) {
 */
 function openCreate() {
     // Se abre la caja de diálogo que contiene el formulario.
-    
     // Se restauran los elementos del formulario.
     SAVE_FORM.reset();
-    // Se asigna título a la caja de diálogo.
-    MODAL_TITLE.textContent = 'Ingresar una clasificacion';
+    // Se asigna el título a la caja de diálogo.
+    MODAL_TITLE.textContent = 'Crear evento';
+    // Llamada a la función para llenar el select del formulario. Se encuentra en el archivo components.js
+    fillSelect(TIPOEVENTO_API, 'readAll', 'tipo');
+
+
 }
 
 /*
@@ -123,21 +130,30 @@ function openCreate() {
 *   Retorno: ninguno.
 */
 async function openUpdate(id) {
-    // Se define una constante tipo objeto con los datos del registro seleccionado.
+    // Se define un objeto con los datos del registro seleccionado.
     const FORM = new FormData();
-    FORM.append('idclasificacion_deporte', id);
+    FORM.append('id', id);
     // Petición para obtener los datos del registro solicitado.
-    const JSON = await dataFetch(CLASIFICACION_API, 'readOne', FORM);
+    const JSON = await dataFetch(EVENTO_API, 'readOne', FORM);
     // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
     if (JSON.status) {
         // Se abre la caja de diálogo que contiene el formulario.
         // Se restauran los elementos del formulario.
         SAVE_FORM.reset();
-        // Se asigna título a la caja de diálogo.
-        MODAL_TITLE.textContent = 'Actualizar clasificacion';
+        // Se asigna el título para la caja de diálogo (modal).
+        MODAL_TITLE.textContent = 'Actualizar evento';
         // Se inicializan los campos del formulario.
-        document.getElementById('id').value = JSON.dataset.idclasificacion_deporte;
-        document.getElementById('nombre').value = JSON.dataset.nombre_clasificacion;
+        document.getElementById('id').value = JSON.dataset.idevento;
+        fillSelect(TIPOEVENTO_API, 'readAll', 'tipo', JSON.dataset.tipo);
+        document.getElementById('nombre').value = JSON.dataset.nombre_evento;
+        document.getElementById('descripcion').value = JSON.dataset.descripcion;
+        document.getElementById('fecha').value = JSON.dataset.fecha_evento;
+        document.getElementById('imagen').value = JSON.dataset.imagen_sede;
+        document.getElementById('sede').value = JSON.dataset.sede_evento;
+        document.getElementById('direccion').value = JSON.dataset.direccion_sede;
+        document.getElementById('inicio').value = JSON.dataset.hora_inicio;
+        document.getElementById('cierre').value = JSON.dataset.hora_cierre;
+
         // Se actualizan los campos para que las etiquetas (labels) no queden sobre los datos.
     } else {
         sweetAlert(2, JSON.exception, false);
@@ -151,14 +167,14 @@ async function openUpdate(id) {
 */
 async function openDelete(id) {
     // Llamada a la función para mostrar un mensaje de confirmación, capturando la respuesta en una constante.
-    const RESPONSE = await confirmAction('¿Desea eliminar la clasificacion de forma permanente?');
+    const RESPONSE = await confirmAction('¿Desea eliminar el evento de forma permanente?');
     // Se verifica la respuesta del mensaje.
     if (RESPONSE) {
         // Se define una constante tipo objeto con los datos del registro seleccionado.
         const FORM = new FormData();
-        FORM.append('idclasificacion_deporte', id);
+        FORM.append('idevento', id);
         // Petición para eliminar el registro seleccionado.
-        const JSON = await dataFetch(CLASIFICACION_API, 'delete', FORM);
+        const JSON = await dataFetch(PRUEBA_API, 'delete', FORM);
         // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
         if (JSON.status) {
             // Se carga nuevamente la tabla para visualizar los cambios.
