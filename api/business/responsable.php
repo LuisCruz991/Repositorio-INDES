bdb51a5e8cd8494bd3b9bc1267accfb2d70adadb
LuisCruz991 <greninjaash1000@gmail.com>
2023-06-22 10:29:50 -0600

    responsables

---
+++ api/business/responsable.php
@@ -0,0 +1,119 @@
+<?php
+require_once('../entities/dto/responsables.php');
+
+// Se comprueba si existe una acción a realizar, de lo contrario se finaliza el script con un mensaje de error.
+if (isset($_GET['action'])) {
+    // Se crea una sesión o se reanuda la actual para poder utilizar variables de sesión en el script.
+    session_start();
+    // Se instancia la clase correspondiente.
+    $responsable = new Responsable;
+    // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
+    $result = array('status' => 0, 'message' => null, 'exception' => null, 'dataset' => null);
+    // Se verifica si existe una sesión iniciada como administrador, de lo contrario se finaliza el script con un mensaje de error.
+    if (isset($_SESSION['idusuario'])) {
+        // Se compara la acción a realizar cuando un administrador ha iniciado sesión.
+        switch ($_GET['action']) {
+            case 'readAll':
+                if ($result['dataset'] = $responsable->readAll()) {
+                    $result['status'] = 1;
+                } elseif (Database::getException()) {
+                    $result['exception'] = Database::getException();
+                } else {
+                    $result['exception'] = 'No hay datos registrados';
+                }
+                break;
+            case 'search':
+                $_POST = Validator::validateForm($_POST);
+                if ($_POST['search'] == '') {
+                    $result['exception'] = 'Ingrese un valor para buscar';
+                } elseif ($result['dataset'] = $responsable->searchRows($_POST['search'])) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Existen '.count($result['dataset']).' coincidencias';
+                } elseif (Database::getException()) {
+                    $result['exception'] = Database::getException();
+                } else {
+                    $result['exception'] = 'No hay coincidencias';
+                }
+                break;
+            case 'create':
+                $_POST = Validator::validateForm($_POST);
+                if (!$responsable->setNombreMadre($_POST['nombre_madre'])) {
+                    $result['exception'] = 'Nombre de la madre incorrecto';
+                }  elseif (!$responsable->setDireccionMadre($_POST['direccion_madre'])) {
+                    $result['exception'] = 'Direccion incorrecta';
+                }  elseif (!$responsable->setTelefonoMadre($_POST['telefono_madre'])) {
+                    $result['exception'] = 'Telefono incorrecto';
+                }  elseif (!$responsable->setNombrePadre($_POST['nombre_padre'])) {
+                    $result['exception'] = 'Nombre del padre incorrecto';
+                }  elseif (!$responsable->setDireccionPadre($_POST['direccion_padre'])) {
+                    $result['exception'] = 'Direccion incorrecta';
+                }  elseif (!$responsable->setTelefonoPadre($_POST['telefono_padre'])) {
+                    $result['exception'] = 'Telefono incorrecto';
+                }  elseif ($responsable->createRow()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Responsable agregado correctamente';
+                } else {
+                    $result['exception'] = Database::getException();;
+                }
+                break;
+            case 'readOne':
+                if (!$responsable->setId($_POST['id'])) {
+                    $result['exception'] = 'Responsable incorrecto';
+                } elseif ($result['dataset'] = $responsable->readOne()) {
+                    $result['status'] = 1;
+                } elseif (Database::getException()) {
+                    $result['exception'] = Database::getException();
+                } else {
+                    $result['exception'] = 'Responsable inexistente';
+                }
+                break;
+            case 'update':
+                $_POST = Validator::validateForm($_POST);
+                if (!$responsable->setId($_POST['id'])) {
+                    $result['exception'] = 'Prueba incorrecta';
+                } elseif (!$data = $responsable->readOne()) {
+                    $result['exception'] = 'Prueba inexistente';
+                } elseif (!$responsable->setNombreMadre($_POST['nombre_madre'])) {
+                    $result['exception'] = 'Nombre de la madre incorrecto';
+                } elseif (!$responsable->setDireccionMadre($_POST['direccion_madre'])) {
+                    $result['exception'] = 'Direccion incorrecta';
+                } elseif (!$responsable->setTelefonoMadre($_POST['telefono_madre'])) {
+                    $result['exception'] = 'Telefono incorrecito';
+                } elseif (!$responsable->setNombrePadre($_POST['nombre_padre'])) {
+                    $result['exception'] = 'Nombre del padre incorrecto';
+                } elseif (!$responsable->setDireccionPadre($_POST['direccion_padre'])) {
+                    $result['exception'] = 'Nombre de la madre incorrecto';
+                } elseif (!$responsable->setTelefonoPadre($_POST['telefono_padre'])) {
+                    $result['exception'] = 'Telefono incorrecto';
+                } elseif ($responsable->updateRow()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Responsables modificados correctamente';
+                }else {
+                    $result['exception'] = Database::getException();
+                }
+                break;
+            case 'delete':
+                if (!$responsable->setId($_POST['idresponsable'])) {
+                    $result['exception'] = 'Responsable incorrecto';
+                } elseif (!$responsable->readOne()) {
+                    $result['exception'] = 'Responsable inexistente';
+                } elseif ($responsable->deleteRow()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Responsables eliminados correctamente';
+                } else {
+                    $result['exception'] = Database::getException();
+                }
+                break;
+            default:
+                $result['exception'] = 'Acción no disponible dentro de la sesión';
+        }
+        // Se indica el tipo de contenido a mostrar y su respectivo conjunto de caracteres.
+        header('content-type: application/json; charset=utf-8');
+        // Se imprime el resultado en formato JSON y se retorna al controlador.
+        print(json_encode($result));
+    } else {
+        print(json_encode('Acceso denegado'));
+    }
+} else {
+    print(json_encode('Recurso no disponible'));
+}
--- controllers/responsables.js
+++ controllers/responsables.js
@@ -1,5 +1,5 @@
 // Constante para completar la ruta de la API.
-const TALLA_API = 'business/dashboard/talla.php';
+const RESPONSABLE_API = 'business/dashboard/responsable.php';
 // Constante para establecer el formulario de buscar.
 const SEARCH_FORM = document.getElementById('search-form');
 // Constante para establecer el formulario de guardar.
@@ -43,7 +43,7 @@ SAVE_FORM.addEventListener('submit', async (event) => {
     // Constante tipo objeto con los datos del formulario.
     const FORM = new FormData(SAVE_FORM);
     // Petición para guardar los datos del formulario.
-    const JSON = await dataFetch(TALLA_API, action, FORM);
+    const JSON = await dataFetch(RESPONSABLE_API, action, FORM);
     // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
     if (JSON.status) {
         // Se carga nuevamente la tabla para visualizar los cambios.
@@ -69,7 +69,7 @@ async function fillTable(form = null) {
     // Se verifica la acción a realizar.
     (form) ? action = 'search' : action = 'readAll';
     // Petición para obtener los registros disponibles.
-    const JSON = await dataFetch(TALLA_API, action, form);
+    const JSON = await dataFetch(RESPONSABLE_API, action, form);
     // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
     if (JSON.status) {
         // Se recorre el conjunto de registros fila por fila.
@@ -77,17 +77,22 @@ async function fillTable(form = null) {
             // Se crean y concatenan las filas de la tabla con los datos de cada registro.
             TBODY_ROWS.innerHTML += `
                 <tr>
-                    <td>${row.idtalla}</td>
-                    <td>${row.num_talla}</td>
+                    <td>${row.idresponsable}</td>
+                    <td>${row.nombre_madre}</td>
+                    <td>${row.direccion_madre}</td>
+                    <td>${row.telefono_madre}</td>
+                    <td>${row.nombre_padre}</td>
+                    <td>${row.direccion_padre}</td>
+                    <td>${row.telefono_padre}</td>
                     <td>
-                        <a onclick="openUpdate(${row.idtalla})" data-bs-toggle="modal" data-bs-target="#save-modal" class="btn btn-primary tooltipped" data-tooltip="Actualizar">
+                        <a onclick="openUpdate(${row.idresponsable})" data-bs-toggle="modal" data-bs-target="#save-modal" class="btn btn-primary tooltipped" data-tooltip="Actualizar">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-recycle" viewBox="0 0 16 16">
                         <path d="M9.302 1.256a1.5 1.5 0 0 0-2.604 0l-1.704 2.98a.5.5 0 0 0 .869.497l1.703-2.981a.5.5 0 0 1 .868 0l2.54 4.444-1.256-.337a.5.5 0 1 0-.26.966l2.415.647a.5.5 0 0 0 .613-.353l.647-2.415a.5.5 0 1 0-.966-.259l-.333 1.242-2.532-4.431zM2.973 7.773l-1.255.337a.5.5 0 1 1-.26-.966l2.416-.647a.5.5 0 0 1 .612.353l.647 2.415a.5.5 0 0 1-.966.259l-.333-1.242-2.545 4.454a.5.5 0 0 0 .434.748H5a.5.5 0 0 1 0 1H1.723A1.5 1.5 0 0 1 .421 12.24l2.552-4.467zm10.89 1.463a.5.5 0 1 0-.868.496l1.716 3.004a.5.5 0 0 1-.434.748h-5.57l.647-.646a.5.5 0 1 0-.708-.707l-1.5 1.5a.498.498 0 0 0 0 .707l1.5 1.5a.5.5 0 1 0 .708-.707l-.647-.647h5.57a1.5 1.5 0 0 0 1.302-2.244l-1.716-3.004z"/>
                         </svg>
                         </a>
                     </td> 
                     <td>
-                        <a onclick="openDelete(${row.idtalla})" class="btn btn-danger tooltipped" data-tooltip="Eliminar">
+                        <a onclick="openDelete(${row.idresponsable})" class="btn btn-danger tooltipped" data-tooltip="Eliminar">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                         <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                         </svg>
@@ -114,7 +119,7 @@ function openCreate() {
     // Se restauran los elementos del formulario.
     SAVE_FORM.reset();
     // Se asigna título a la caja de diálogo.
-    MODAL_TITLE.textContent = 'Ingresar talla';
+    MODAL_TITLE.textContent = 'Ingresar responsable';
 }
 
 /*
@@ -125,19 +130,24 @@ function openCreate() {
 async function openUpdate(id) {
     // Se define una constante tipo objeto con los datos del registro seleccionado.
     const FORM = new FormData();
-    FORM.append('idtalla', id);
+    FORM.append('idresponsable', id);
     // Petición para obtener los datos del registro solicitado.
-    const JSON = await dataFetch(TALLA_API, 'readOne', FORM);
+    const JSON = await dataFetch(RESPONSABLE_API, 'readOne', FORM);
     // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
     if (JSON.status) {
         // Se abre la caja de diálogo que contiene el formulario.
         // Se restauran los elementos del formulario.
         SAVE_FORM.reset();
         // Se asigna título a la caja de diálogo.
-        MODAL_TITLE.textContent = 'Actualizar talla';
+        MODAL_TITLE.textContent = 'Actualizar responsables';
         // Se inicializan los campos del formulario.
-        document.getElementById('id').value = JSON.dataset.idtalla;
-        document.getElementById('numero').value = JSON.dataset.num_talla;
+        document.getElementById('id').value = JSON.dataset.idresponsable;
+        document.getElementById('nombre_madre').value = JSON.dataset.nombre_madre;
+        document.getElementById('direccion_madre').value = JSON.dataset.direccion_madre;
+        document.getElementById('telefono_madre').value = JSON.dataset.telefono_madre;
+        document.getElementById('nombre_padre').value = JSON.dataset.nombre_padre;
+        document.getElementById('direccion_padre').value = JSON.dataset.direccion_padre;
+        document.getElementById('telefono_padre').value = JSON.dataset.telefono_padre;
         // Se actualizan los campos para que las etiquetas (labels) no queden sobre los datos.
     } else {
         sweetAlert(2, JSON.exception, false);
@@ -151,14 +161,14 @@ async function openUpdate(id) {
 */
 async function openDelete(id) {
     // Llamada a la función para mostrar un mensaje de confirmación, capturando la respuesta en una constante.
-    const RESPONSE = await confirmAction('¿Desea eliminar la talla de forma permanente?');
+    const RESPONSE = await confirmAction('¿Desea eliminar los responsables de forma permanente?');
     // Se verifica la respuesta del mensaje.
     if (RESPONSE) {
         // Se define una constante tipo objeto con los datos del registro seleccionado.
         const FORM = new FormData();
-        FORM.append('idtalla', id);
+        FORM.append('idresponsable', id);
         // Petición para eliminar el registro seleccionado.
-        const JSON = await dataFetch(TALLA_API, 'delete', FORM);
+        const JSON = await dataFetch(RESPONSABLE_API, 'delete', FORM);
         // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
         if (JSON.status) {
             // Se carga nuevamente la tabla para visualizar los cambios.
