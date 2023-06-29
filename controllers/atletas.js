4ea194d320468cb1d050b55d6333da3f00e49d72
Walter Calderon <87783472+WalterCalderon123@users.noreply.github.com>
2023-06-29 11:03:49 -0600

    Dashboard
    dashboard

--- controllers/atletas.js
+++ controllers/atletas.js
@@ -1,68 +1,52 @@
-// Constante para completar la ruta de la API.
+// Ruta de la API para los atletas.
 const ATLETA_API = 'business/atleta.php';
+// Ruta de la API para los responsables.
 const RESPONSABLE_API = 'business/responsable.php';
+// Ruta de la API para los deportes.
 const DEPORTE_API = 'business/deporte.php';
 
-// Constante para establecer el formulario de buscar.
+// Formulario de búsqueda.
 const SEARCH_FORM = document.getElementById('search-form');
-// Constante para establecer el formulario de guardar.
+// Formulario de guardar.
 const SAVE_FORM = document.getElementById('save-form');
-// Constante para establecer el título de la modal.
+// Título de la modal.
 const MODAL_TITLE = document.getElementById('modal-title');
-// Constantes para establecer el contenido de la tabla.
+// Contenido de la tabla.
 const TBODY_ROWS = document.getElementById('tbody-rows');
 const RECORDS = document.getElementById('records');
 
-// Inicialización del componente Modal para que funcionen las cajas de diálogo.
-
-// Constante para establecer la modal de guardar.
+// Modal de guardar.
 const SAVE_MODAL = new Modal(document.getElementById('save-modal'));
 
-// Método manejador de eventos para cuando el documento ha cargado.
+// Evento cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', () => {
-    // Llamada a la función para llenar la tabla con los registros disponibles.
+    // Llenar la tabla con los registros disponibles.
     fillTable();
 });
 
-// Método manejador de eventos para cuando se envía el formulario de buscar.
+// Evento cuando se envía el formulario de búsqueda.
 SEARCH_FORM.addEventListener('submit', (event) => {
-    // Se evita recargar la página web después de enviar el formulario.
-    event.preventDefault();
-    // Constante tipo objeto con los datos del formulario.
-    const FORM = new FormData(SEARCH_FORM);
-    // Llamada a la función para llenar la tabla con los resultados de la búsqueda.
-    fillTable(FORM);
+    event.preventDefault(); // Evitar recargar la página web después de enviar el formulario.
+    const FORM = new FormData(SEARCH_FORM); // Datos del formulario.
+    fillTable(FORM); // Llenar la tabla con los resultados de la búsqueda.
 });
 
-// Método manejador de eventos para cuando se envía el formulario de guardar.
+// Evento cuando se envía el formulario de guardar.
 SAVE_FORM.addEventListener('submit', async (event) => {
-    // Se evita recargar la página web después de enviar el formulario.
-    event.preventDefault();
-    // Se verifica la acción a realizar.
-    (document.getElementById('id').value) ? action = 'update' : action = 'create';
-    // Constante tipo objeto con los datos del formulario.
-    const FORM = new FormData(SAVE_FORM);
-    // Petición para guardar los datos del formulario.
-    const JSON = await dataFetch(ATLETA_API, action, FORM);
-    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    event.preventDefault(); // Evitar recargar la página web después de enviar el formulario.
+    let action = (document.getElementById('id').value) ? 'update' : 'create'; // Verificar la acción a realizar.
+    const FORM = new FormData(SAVE_FORM); // Datos del formulario.
+    const JSON = await dataFetch(ATLETA_API, action, FORM); // Guardar los datos del formulario.
     if (JSON.status) {
-        // Se carga nuevamente la tabla para visualizar los cambios.
-        fillTable();
-        // Se cierra la caja de diálogo.
-        SAVE_MODAL.toggle();
-        // Se muestra un mensaje de éxito.
-        sweetAlert(1, JSON.message, true);
-
+        fillTable(); // Cargar la tabla nuevamente para visualizar los cambios.
+        SAVE_MODAL.toggle(); // Cerrar la caja de diálogo.
+        sweetAlert(1, JSON.message, true); // Mostrar un mensaje de éxito.
     } else {
-        sweetAlert(2, JSON.exception, false);
+        sweetAlert(2, JSON.exception, false); // Mostrar un mensaje de error.
     }
 });
 
-/*
-*   Función asíncrona para llenar la tabla con los registros disponibles.
-*   Parámetros: form (objeto opcional con los datos de búsqueda).
-*   Retorno: ninguno.
-*/
+// Llenar la tabla con los registros disponibles.
 async function fillTable(form = null) {
     // Se inicializa el contenido de la tabla.
     TBODY_ROWS.innerHTML = '';
@@ -166,90 +150,44 @@ async function fillTable(form = null) {
         sweetAlert(4, JSON.exception, true);
     }
 }
-
-/*
-*   Función para preparar el formulario al momento de insertar un registro.
-*   Parámetros: ninguno.
-*   Retorno: ninguno.
-*/
+// Preparar el formulario al momento de insertar un registro.
 function openCreate() {
-    // Se abre la caja de diálogo que contiene el formulario.
-
-    // Se restauran los elementos del formulario.
-    SAVE_FORM.reset
-    // Se asigna título a la caja de diálogo.
+    // Abrir la caja de diálogo que contiene el formulario.
+    SAVE_FORM.reset(); // Restaurar los elementos del formulario.
     fillSelect(ATLETA_API, 'readGenero', 'genero');
     fillSelect(RESPONSABLE_API, 'readAll', 'nombre_madre');
     fillSelect(DEPORTE_API, 'readAll', 'deporte');
     fillSelect(ATLETA_API, 'readEntrenador', 'entrenador');
 }
 
-/*
-*   Función asíncrona para preparar el formulario al momento de actualizar un registro.
-*   Parámetros: id (identificador del registro seleccionado).
-*   Retorno: ninguno.
-*/
+// Preparar el formulario al momento de actualizar un registro.
 async function openUpdate(id) {
-    // Se define una constante tipo objeto con los datos del registro seleccionado.
     const FORM = new FormData();
     FORM.append('idatleta', id);
-    // Petición para obtener los datos del registro solicitado.
-    const JSON = await dataFetch(ATLETA_API, 'readOne', FORM);
-    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    const JSON = await dataFetch(ATLETA_API, 'readOne', FORM); // Obtener los datos del registro seleccionado.
     if (JSON.status) {
-        // Se abre la caja de diálogo que contiene el formulario.
-        SAVE_MODAL.show();
-        // Se restauran los elementos del formulario.
-        SAVE_FORM.reset();
-        // Se asigna título a la caja de diálogo.
-        // Se inicializan los campos del formulario.
+        SAVE_MODAL.show(); // Abrir la caja de diálogo que contiene el formulario.
+        SAVE_FORM.reset(); // Restaurar los elementos del formulario.
+        // Inicializar los campos del formulario.
         document.getElementById('id').value = JSON.dataset.idatleta;
-        document.getElementById('nombre').value = JSON.dataset.nombre_atleta;
-        document.getElementById('apellido').value = JSON.dataset.apellido_atleta;
-        document.getElementById('nacimiento').value = JSON.dataset.nacimiento;
-        fillSelect(ATLETA_API, 'readGenero', 'genero', JSON.dataset.idgenero);
-        document.getElementById('estatura').value = JSON.dataset.estatura;
-        document.getElementById('peso').value = JSON.dataset.peso;
-        document.getElementById('camisa').value = JSON.dataset.talla_camisa;
-        document.getElementById('short').value = JSON.dataset.talla_short;
-        document.getElementById('direccion').value = JSON.dataset.direccion;
-        document.getElementById('dui').value = JSON.dataset.dui;
-        document.getElementById('celular').value = JSON.dataset.celular;
-        document.getElementById('telefono').value = JSON.dataset.telefono_casa;
-        document.getElementById('correo').value = JSON.dataset.correo;
-        fillSelect(RESPONSABLE_API, 'readAll', 'nombre_madre', JSON.dataset.idresponsable);
-        fillSelect(DEPORTE_API, 'readAll', 'deporte', JSON.dataset.iddeporte);
-        fillSelect(ATLETA_API, 'readEntrenador', 'entrenador', JSON.dataset.identrenador);
-        document.getElementById('clave').disabled = true ;
-        // Se actualizan los campos para que las etiquetas (labels) no queden sobre los datos.
+        document.getElementById('clave').disabled = true;
     } else {
-        sweetAlert(2, JSON.exception, false);
+        sweetAlert(2, JSON.exception, false); // Mostrar un mensaje de error.
     }
 }
 
-/*
-*   Función asíncrona para eliminar un registro.
-*   Parámetros: id (identificador del registro seleccionado).
-*   Retorno: ninguno.
-*/
+// Eliminar un registro.
 async function openDelete(id) {
-    // Llamada a la función para mostrar un mensaje de confirmación, capturando la respuesta en una constante.
     const RESPONSE = await confirmAction('¿Desea eliminar los responsables de forma permanente?');
-    // Se verifica la respuesta del mensaje.
     if (RESPONSE) {
-        // Se define una constante tipo objeto con los datos del registro seleccionado.
         const FORM = new FormData();
         FORM.append('idatleta', id);
-        // Petición para eliminar el registro seleccionado.
-        const JSON = await dataFetch(ATLETA_API, 'delete', FORM);
-        // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+        const JSON = await dataFetch(ATLETA_API, 'delete', FORM); // Eliminar el registro seleccionado.
         if (JSON.status) {
-            // Se carga nuevamente la tabla para visualizar los cambios.
-            fillTable();
-            // Se muestra un mensaje de éxito.
-            sweetAlert(1, JSON.message, true);
+            fillTable(); // Cargar la tabla nuevamente para visualizar los cambios.
+            sweetAlert(1, JSON.message, true); // Mostrar un mensaje de éxito.
         } else {
-            sweetAlert(2, JSON.exception, false);
+            sweetAlert(2, JSON.exception, false); // Mostrar un mensaje de error.
         }
     }
 }
