6052536079ed806bf98de088f75d3fcb27415205
jimmy <20210637@ricaldone.edu.sv>
2023-08-11 17:36:36 -0600

    parentesco crud

---
+++ controllers/parentesco.js
@@ -0,0 +1,177 @@
+// Constante para completar la ruta de la API.
+const PARENTESCO_API = 'business/parentesco.php';
+// Constante para establecer el formulario de buscar.
+const SEARCH_FORM = document.getElementById('search-form');
+// Constante para establecer el formulario de guardar.
+const SAVE_FORM = document.getElementById('save-form');
+// Constantes para establecer el contenido de la tabla.
+const TBODY_ROWS = document.getElementById('tbody-rows');
+const RECORDS = document.getElementById('records');
+
+
+// Constante para establecer la modal de guardar.
+const SAVE_MODAL = new Modal(document.getElementById('save-modal'));
+
+
+// Método manejador de eventos para cuando el documento ha cargado.
+document.addEventListener('DOMContentLoaded', () => {
+    // Llamada a la función para llenar la tabla con los registros disponibles.
+    fillTable();
+});
+
+// Método manejador de eventos para cuando se envía el formulario de buscar.
+SEARCH_FORM.addEventListener('submit', (event) => {
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Constante tipo objeto con los datos del formulario.
+    const FORM = new FormData(SEARCH_FORM);
+    // Llamada a la función para llenar la tabla con los resultados de la búsqueda.
+    fillTable(FORM);
+});
+
+// Método manejador de eventos para cuando se envía el formulario de guardar.
+SAVE_FORM.addEventListener('submit', async (event) => {
+    // Se evita recargar la página web después de enviar el formulario.
+    event.preventDefault();
+    // Se verifica la acción a realizar.
+    (document.getElementById('id').value) ? action = 'update' : action = 'create';
+    // Constante tipo objeto con los datos del formulario.
+    const FORM = new FormData(SAVE_FORM);
+    // Petición para guardar los datos del formulario.
+    const JSON = await dataFetch(PARENTESCO_API, action, FORM);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (JSON.status) {
+        // Se carga nuevamente la tabla para visualizar los cambios.
+        fillTable();
+        // Se muestra un mensaje de éxito.
+        sweetAlert(1, JSON.message, true);
+    } else {
+        sweetAlert(2, JSON.exception, false);
+    }
+});
+
+/*
+*   Función para buscar datos al presionar una tecla.
+*   Parámetros: ninguno.
+*   Retorno: ninguno.
+*/
+function searchRows() {
+    value = document.getElementById('search').value;
+    if (value) {
+        const FORM = new FormData();
+        FORM.append('search', value);
+        fillTable(FORM);
+    } else {
+        fillTable();
+    }
+}
+
+/*
+*   Función asíncrona para llenar la tabla con los registros disponibles.
+*   Parámetros: form (objeto opcional con los datos de búsqueda).
+*   Retorno: ninguno.
+*/
+async function fillTable(form = null) {
+    // Se inicializa el contenido de la tabla.
+    TBODY_ROWS.innerHTML = '';
+    RECORDS.textContent = '';
+    // Se verifica la acción a realizar.
+    (form) ? action = 'search' : action = 'readAll';
+    // Petición para obtener los registros disponibles.
+    const JSON = await dataFetch(PARENTESCO_API, action, form);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (JSON.status) {
+        // Se recorre el conjunto de registros fila por fila.
+        JSON.dataset.forEach(row => {
+            // Se crean y concatenan las filas de la tabla con los datos de cada registro.
+            TBODY_ROWS.innerHTML += `
+            <tr
+            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              <p class="text-base">${row.idparentesco}</p>
+            </th>
+            <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              <p class="text-center  w-14 rounded-xl">${row.nombre_parentesco}</p>
+            </td>
+            <td >
+              <button data-modal-toggle = "save-modal" class="rounded-md w-24 h-8 bg-btnactualizar-color font-medium text-btnactualizar-texto dark:text-blue-500 hover:underline" onclick="openUpdate(${row.idmodalidad_deporte})">
+                Actualizar
+              </button>
+            <td class="px-6 py-4">
+              <button
+                class=" rounded-md w-24 h-8 bg-red-500 font-medium text-white dark:text-blue-500 hover:underline" onclick="openDelete(${row.idmodalidad_deporte})">
+                Eliminar</a>
+              </button>
+            </td>
+          </tr>
+            `;
+        });
+        RECORDS.textContent = JSON.message;
+    } else {
+        sweetAlert(4, JSON.exception, true);
+    }
+}
+
+/*
+*   Función para preparar el formulario al momento de insertar un registro.
+*   Parámetros: ninguno.
+*   Retorno: ninguno.
+*/
+function openCreate() {
+    // Se restauran los elementos del formulario.
+    SAVE_FORM.reset();
+}
+
+/*
+*   Función asíncrona para preparar el formulario al momento de actualizar un registro.
+*   Parámetros: id (identificador del registro seleccionado).
+*   Retorno: ninguno.
+*/
+async function openUpdate(id) {
+    // Se define una constante tipo objeto con los datos del registro seleccionado.
+    const FORM = new FormData();
+    FORM.append('id', id);
+    // // Se abre la caja de diálogo que contiene el formulario.
+    SAVE_MODAL.show();
+    // Se restauran los elementos del formulario.
+    SAVE_FORM.reset();
+    // Petición para obtener los datos del registro solicitado.
+    const JSON = await dataFetch(PARENTESCO_API, 'readOne', FORM);
+    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+    if (JSON.status) {
+        // Se restauran los elementos del formulario.
+        SAVE_FORM.reset();
+        // Se inicializan los campos del formulario.
+        document.getElementById('id').value = JSON.dataset.idparentesco;
+        document.getElementById('parentesco').value = JSON.dataset.nombre_parentesco;
+    } else {
+        sweetAlert(2, JSON.exception, false);
+    }
+}
+
+/*
+*   Función asíncrona para eliminar un registro.
+*   Parámetros: id (identificador del registro seleccionado).
+*   Retorno: ninguno.
+*/
+async function openDelete(id) {
+    // Llamada a la función para mostrar un mensaje de confirmación, capturando la respuesta en una constante.
+    const RESPONSE = await confirmAction('¿Desea descartar este parentesco?');
+    // Se verifica la respuesta del mensaje.
+    if (RESPONSE) {
+        // Se define una constante tipo objeto con los datos del registro seleccionado.
+        const FORM = new FormData();
+        FORM.append('id', id);
+        // Petición para eliminar el registro seleccionado.
+        const JSON = await dataFetch(PARENTESCO_API, 'delete', FORM);
+        // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+        if (JSON.status) {
+            // Se carga nuevamente la tabla para visualizar los cambios.
+            fillTable();
+            // Se muestra un mensaje de éxito.
+            sweetAlert(1, JSON.message, true);
+        } else {
+            sweetAlert(2, JSON.exception, false);
+        }
+    }
+}
