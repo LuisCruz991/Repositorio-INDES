e814dd33a8f2d6e9b3a68049b41ed508ac2f2690
Walter Calderon <87783472+WalterCalderon123@users.noreply.github.com>
2023-09-07 08:24:30 -0600

    Mostrar contra

--- controllers/account.js
+++ controllers/account.js
@@ -95,7 +95,7 @@ document.addEventListener('DOMContentLoaded', async () => {
             </li>
             <!-- Boton para acceder a atletas -->
             <li class="min-w-max">
-              <button id="dropdownMenuIconButton" data-dropdown-toggle="dropdownDots"
+               <button aria-controls="dropdownatleta" data-collapse-toggle="dropdownatleta"  onclick="const dropdown = new Dropdown(document.getElementById('dropdownatleta')).toggle()"
                 class=" peer space-y-1 bg-azul-3  w-[6.5rem] h-[4.8rem] group flex flex-col items-center justify-center rounded-lg  py-1 text-white ">
                 <svg class="h-10  w-10" fill="currentcolor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                   <title>run</title>
@@ -103,12 +103,12 @@ document.addEventListener('DOMContentLoaded', async () => {
                     d="M13.5,5.5C14.59,5.5 15.5,4.58 15.5,3.5C15.5,2.38 14.59,1.5 13.5,1.5C12.39,1.5 11.5,2.38 11.5,3.5C11.5,4.58 12.39,5.5 13.5,5.5M9.89,19.38L10.89,15L13,17V23H15V15.5L12.89,13.5L13.5,10.5C14.79,12 16.79,13 19,13V11C17.09,11 15.5,10 14.69,8.58L13.69,7C13.29,6.38 12.69,6 12,6C11.69,6 11.5,6.08 11.19,6.08L6,8.28V13H8V9.58L9.79,8.88L8.19,17L3.29,16L2.89,18L9.89,19.38Z" />
                 </svg>
                 <span class="text-sm  font-medium">Atletas</span>
-              </button>
+              </button>d
             </li>
             <!-- Dropdown menu de boton de atletas -->
-            <div id="dropdownDots"
+            <div id="dropdownatleta"
               class="z-10 hidden bg-azul-opaco divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-              <ul class="py-2 text-sm text-white-700 text-red" aria-labelledby="dropdownMenuIconButton">
+              <ul class="py-2 text-sm text-white-700 text-red" aria-labelledby="dropdownatleta">
                 <!-- Boton para acceder a SCRUD de atletas -->
                 <li>
                   <a href="../vistas/atletas_crud.html"
