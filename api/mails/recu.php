1a331cdf5b88398a8c42d539329078f2ced50d52
JosuePortillo1 <82895807+JosuePortillo1@users.noreply.github.com>
2023-09-20 09:42:27 -0600

    recuperacion pro gmail al 90%

--- api/mails/recu.php
+++ api/mails/recu.php
@@ -16,47 +16,55 @@ require_once('../entities/dto/usuario.php');
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
-// se verifica que los parametros enviados no sean nulos
-if (isset($_GET['nombre_usuario']) && isset($_GET['codigo'])) {
-    // se crea el objeto usuario 
-    $user = new Usuario;
-    // le asignamos el valor de del nombre del usuario 
-    if ($user->setAlias($_GET['nombre_usuario'])) {
-        // obtenemos los valores de la consulta 
-        if ($data = $user -> readByname()) {
-            // asignamos el valor del codigo a una variable 
-            $code = $_GET['codigo'];
-            // se ejecuta el codigo de la libreria 
-            try {
-                //Server settings
-                $mail->SMTPDebug = 0; //Enable verbose debug output
-                $mail->isSMTP(); //Send using SMTP
-                $mail->Host = 'smtp.gmail.com'; //Set the SMTP server to send through
-                $mail->SMTPAuth = true; //Enable SMTP authentication
-                $mail->Username = 'josue.portillo0123@gmail.com'; //SMTP username
-                $mail->Password = 'egzi ugui bwms teph '; //SMTP password
-                $mail->SMTPSecure = 'ssl'; //Enable implicit TLS encryption
-                $mail->Port = 465; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-                //Recipients
-                $mail->setFrom('josue.portillo0123@gmail.com', 'Soporte INDES'); // Direccion de correo de donde se envia el correo
-                $mail->addAddress($data['correo_usuario']); //Direccion de correo a dode se envia el correo
-
-                //Content
-                $mail->isHTML(true); //Set email format to HTML
-                $mail->Subject = 'Tu codigo de verificacion';
-                $mail->Body = " $code";
-
-                $mail->send();
-                echo 'Mensaje envido de forma exitosa';
-            } catch (Exception $e) {
-                echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+$user = new Usuario;
+// le asignamos el valor de del nombre del usuario 
+if ($user->setAlias($_GET['nombre_usuario'])) {
+    // obtenemos los valores de la consulta 
+    if ($data = $user->readByname()) {
+        // Funcion para generar el codigo de verificacion 
+        function getCode($n)
+        {
+            $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            $randomString = '';
+            for ($i = 0; $i < $n; $i++) {
+                $index = rand(0, strlen($characters) - 1);
+                $randomString .= $characters[$index];
             }
-        } else {
-            echo 'ha ocurrido un error leyendo los datos del usuario';
-        }
-    }
-} else {
-    echo 'El nombre de usuario ingresado no es valido';
 
+            return $randomString;
+        }
+        // Variable del codigo
+        $code = getCode($n = 8);
+        // se imprime el codigo en formato json
+        echo json_encode(array('code' =>$code));
+        // se ejecuta el codigo de la libreria 
+        try {
+
+            //Server settings
+            $mail->SMTPDebug = 0; //Enable verbose debug output
+            $mail->isSMTP(); //Send using SMTP
+            $mail->Host = 'smtp.gmail.com'; //Set the SMTP server to send through
+            $mail->SMTPAuth = true; //Enable SMTP authentication
+            $mail->Username = 'josue.portillo0123@gmail.com'; //SMTP username
+            $mail->Password = 'egzi ugui bwms teph'; //SMTP password
+            $mail->SMTPSecure = 'ssl'; //Enable implicit TLS encryption
+            $mail->Port = 465; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+            //Recipients
+            $mail->setFrom('josue.portillo0123@gmail.com', 'Soporte INDES'); // Direccion de correo de donde se envia el correo
+            $mail->addAddress($data['correo_usuario']); //Direccion de correo a dode se envia el correo
+
+            //Content
+            $mail->isHTML(true); //Set email format to HTML
+            $mail->Subject = 'Tu codigo de verificacion';
+            $mail->Body = "Tu codigo de verificación: $code";
+
+            $mail->send();
+            // echo 'Mensaje envido de forma exitosa';
+        } catch (Exception $e) {
+            // echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+    } else {
+        echo 'ha ocurrido un error leyendo los datos del usuario';
+    }
 }
--- controllers/recu.js
+++ controllers/recu.js
@@ -1,16 +1,12 @@
 
+// constante con el link del archivo de php
+const RECU = '../api/mails/recu.php';
+
 // se declara la constante del formulario donde se introduce el codigo 
 const CODE_FORM = document.getElementById('code-form');
 
 // se declara la constante del form de recuperacion 
 const RECU_FORM = document.getElementById('recu-form');
-
-
-const chara1 = Math.random().toString(36).substring(2, 3); // se declara el primer valor del codigo
-const chara2 = Math.random().toString(36).substring(2, 3); // se declara el segundo valor del codigo
-const chara3 = Math.random().toString(36).substring(2, 3); // se declara el tercer valor del codigo
-const chara4 = Math.random().toString(36).substring(2, 3); // se declara el cuarto valor del codigo
-
 
 // Evento cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', () => {
@@ -21,7 +17,6 @@ RECU_FORM.addEventListener('submit', async (event) => {
     event.preventDefault(); // Evitar recargar la página web después de enviar el formulario.
 })
 
-
 //  Evento para validar el codigo enviado 
 CODE_FORM.addEventListener('submit', async (event) => {
     event.preventDefault(); // Evitar recargar la página web después de enviar el formulario.
@@ -30,28 +25,25 @@ CODE_FORM.addEventListener('submit', async (event) => {
     var value2 = document.getElementById('c2').value;
     var value3 = document.getElementById('c3').value;
     var value4 = document.getElementById('c4').value;
+    var value5 = document.getElementById('c5').value;
+    var value6 = document.getElementById('c6').value;
+    var value7 = document.getElementById('c7').value;
+    var value8 = document.getElementById('c8').value;
 
+
+    // constante del codigo de verificacion 
+    const CODE = document.getElementById('code').value ;
     // Metodo para validar que no hayan campos vacios 
-    if (value1 != "" && value2 != "" && value3 != "" && value4 != "") {
-
-        // array donde se almacenan los characteres ingresados 
-        const CODE = new Array (chara1, chara2, chara3, chara4);
-        let character = CODE.toString(); // Convertimos el array del codigo a string
-        const DATA = new Array (value1, value2, value3, value4); // se guardan los characteres ingresados en un array
-        let input = DATA.toString(); // el array lo convertimos a string
+    if (value1 != "" && value2 != "" && value3 != "" && value4 != "" && value5 != "" && value6 != "" && value7 != "" && value8 != "") {
+        const DATA = value1 + value2 + value3 + value4 + value5 + value6 + value7 +value8 ; // se guardan los characteres ingresados en una constante
         // Metodo para comprobar que los dos codigos coincidan 
-        if (character == input) {
+        if (CODE == DATA) {
             // En caso de coincidir se imprime un mensaje y se redirecciona al usuario al formulario de restablecer contraseña
             sweetAlert(1, 'Autenticación completada exitosamente', true, 'index.html');
         }
         else {
             // En caso de no coincidir se imprime un mensaje de error 
             sweetAlert(2, 'El código que has introducido no coincide con el enviado', false); // Mostrar un mensaje de error.
-            // se limpian los campos en caso de error 
-            // var value1 = document.getElementById('c1').value = '';
-            // var value2 = document.getElementById('c2').value = '';
-            // var value3 = document.getElementById('c3').value = '';
-            // var value4 = document.getElementById('c4').value = '';
         }
     }
     else {
@@ -59,23 +51,21 @@ CODE_FORM.addEventListener('submit', async (event) => {
 })
 
 
-
-
 // Funcion para abrir el reporte de las horas cumplidas de entrenamiento de un atleta
-function openMail(name,code) {
-    // varible donde se almacenan los characteres del codigo 
-    code = chara1 + chara2 + chara3 + chara4 ;
+function openMail() {
+    //se ejecuta la sentencia de el php con el paraetro del nombre de usuario
+    fetch(`${RECU}?nombre_usuario=${document.getElementById('nombres').value}`).then(response => response.json())
+    .then(data => {
+        //se le asigna el codigo al campor del codigo
+        document.getElementById('code').value = data.code;
+    }).catch(error => {
+        //se imprime un mensaje de error en tal caso
+        console.error('Ha ocurrido un error al ejecutar el codigo', error);
+    });
 
-    name = document.getElementById('nombres').value ;
-    // Se declara una constante tipo objeto con la ruta específica del reporte en el servidor.
-    const PATH = new URL(`${SERVER_URL}mails/recu.php`);
-    //
-    PATH.searchParams.append('codigo',code);
-    //
-    PATH.searchParams.append('nombre_usuario',name);
-    // Se abre el reporte en una nueva pestaña del navegador web.
-    window.open(PATH.href);
 }
+
+
 
 
 
