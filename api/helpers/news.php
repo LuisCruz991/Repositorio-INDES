17f364e4bd72a8c7cd8f6e733b91be0c21b2a4d5
JosuePortillo1 <82895807+JosuePortillo1@users.noreply.github.com>
2023-10-10 22:32:23 -0600

    Avance del ranking

--- api/helpers/news.php
+++ api/helpers/news.php
@@ -1,9 +1,11 @@
 <?php
 $apiKey = '1748d28b014d44089660a08c1af3f018'; // Reemplaza esto con tu API Key
-$category = 'sports'; // Categoria de la noticia
-$keyword = 'olympic'; // Palabra clave para buscar noticias relacionadas
+$keyword = 'Olympics AND "Tokyo 2020" AND "Olympic Games" OR "Paris 2024"'; // Palabra clave para buscar noticias relacionadas
+$pageSize = '15';
+$page = '1';
+$sortBy = 'relevancy' ;
 
-$url = "https://newsapi.org/v2/top-headlines?category={$category}&q={$keyword}&apiKey={$apiKey}";
+$url = "https://newsapi.org/v2/everything?q={$keyword}&pageSize={$pageSize}&page={$page}&sortBy={$sortBy}&apiKey={$apiKey}";
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
